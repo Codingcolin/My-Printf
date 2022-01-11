17aca9772807b1bdb2317ca687ae6b81db52c1fb
colin jackson <jackson_c@qwasar.io>
2022-01-11 23:51:31 +0000

    printfv2

--- Myprint/my_printf.c
+++ Myprint/my_printf.c
@@ -5,22 +5,27 @@
 #include <stdint.h>
 
 
-void my_putchar(char c) {
-    write(1, &c, 1);
+int my_putchar(char c) {
+ return write(1, &c, 1);
 }
 
-void print_oct(int dec)
+int print_oct(int dec)
   { 
+    int result = 0;  
     if (dec / 8 != 0)
     {
-        print_oct(dec / 8);
+       result = print_oct(dec / 8);
         
     } 
-        my_putchar((dec % 8) + '0');
-  }
+      result += my_putchar((dec % 8) + '0');
+    return result;
+    }
 
-void print_int(int num)
+    
+
+int print_int(int num)
   { 
+    int result = 0;    
     if (num < 0)
     {
         my_putchar('-');
@@ -28,10 +33,11 @@ void print_int(int num)
     }
     if (num / 10 != 0)
     {
-        print_int(num / 10);
+       result = print_int(num / 10);
         
     } 
         my_putchar((num % 10) + '0');
+        return result;
   }
 
 int my_strlen(char* str)
@@ -47,13 +53,13 @@ int my_strlen(char* str)
 }
  
 
-  void put_str(char *str)
+  int put_str(char *str)
 {
     int len = my_strlen(str);
-    write(1, str , len);
+    return write(1, str , len);
 }
 
-void print_hexa(long long num) 
+int print_hexa(long long num) 
 {
     char hexiaDeci[100] = {'\0'};
     int i = 1;
@@ -87,21 +93,24 @@ void print_hexa(long long num)
     
 }
 
-void print_unint(unsigned int num)
+int print_unint(unsigned int num)
   { 
+    int result = 0;  
     if (num / 10 != 0)
     {
-        print_unint(num / 10);
+       result = print_unint(num / 10);
         
     } 
         my_putchar((num % 10) + '0');
+        return result;
   }
 
-void print_ptr(long long ptr) 
+int print_ptr(long long ptr) 
 {
-    
+    int result = 0;
    
-        print_hexa(ptr); 
+     result = print_hexa(ptr); 
+     return result;
 }
 
 int my_printf(char* format, ...)
@@ -109,14 +118,15 @@ int my_printf(char* format, ...)
     //printf("test %s", format);
     va_list arg;
     va_start(arg, format);
-
+    int count = 0;
     int i = 0;
     while(format[i] != '\0') 
     { //Print first string
     //printf("\nThis format %s\n", format);
         if(format[i] != '%')
         {
-            my_putchar(format[i]);
+            count += my_putchar(format[i]);
+            
         }   
         else if(format[i] == '%')
         {
@@ -128,25 +138,25 @@ int my_printf(char* format, ...)
                 
                   
                 case 'c' : value = va_arg(arg, int);
-                    my_putchar(value);
+                   count += my_putchar(value);
                     break;
                 case 's' : string = va_arg(arg, char *);
-                    put_str(string);
+                   count += put_str(string);
                     break;
                 case 'd' : value = va_arg(arg, int);
-                    print_int(value);
+                   count += print_int(value);
                     break;
                 case 'o' : value = va_arg(arg, int);
-                    print_oct(value);
+                   count += print_oct(value);
                     break;
                 case 'u' : value = va_arg(arg, unsigned int);
-                    print_unint(value);
+                   count +=  print_unint(value);
                     break;
                 case 'x' : value = va_arg(arg, unsigned int);
-                    print_hexa(value);
+                   count +=  print_hexa(value);
                     break;     
                case 'p' : value = va_arg(arg,  long long);
-                    put_str("Ox");
+                  count +=  put_str("Ox");
                     print_ptr(value);
                     break;   
                
@@ -155,22 +165,32 @@ int my_printf(char* format, ...)
         i++;
     }
     va_end(arg);
+    return count;
 }
 
 
 
 int main() {
     int a = 0;
-   my_printf("This is a ptr %p\n", &a);
+    int result = 0;
+   result = my_printf("This is a ptr %p\n", &a);
+   printf("size is %d\n", result);
    //printf("This is a ptr %p\n", &a);
    
-   my_printf("This is decimal to hexidecimal conversion %x \n", 2545);
-   my_printf("The octal value of this number is : %o\n", 80);
-   my_printf("This is the decimal %d\n", 10);
-   my_printf("This is the string %s\n", "hey");
-   my_printf("This is the unsigned int %u\n", 2);
-   my_printf("This is the character %c %c\n", 'h', 'e');
-
+    result = my_printf("This is decimal to hexidecimal conversion %x \n", 2545);
+    printf("size is %d\n", result);
+    result = my_printf("The octal value of this number is : %o\n", 80);
+    printf("size is %d\n", result);
+    result = my_printf("This is the decimal %d\n", 10);
+    printf("size is %d\n", result);
+    result = my_printf("This is the string %s\n", "hey");
+    printf("size is %d\n", result);
+    result = my_printf("This is the unsigned int %u\n", 2);
+    printf("size is %d\n", result);
+    result = my_printf("This is the character %c %c\n", 'h', 'e');
+    printf("size is %d\n", result);
+    result = printf("The octal value of this number is : %o\n", 80);
+    printf("result should be %d\n", result);
     return 0;
 }
 
